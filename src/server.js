5cfdc2e81a1a14469eb4ffeae7de5edeb5b9c2dd
정상준 <mornya@wemakeprice.com>
2018-12-11 20:53:20 +0900

    설정변경

--- src/server.js
+++ src/server.js
@@ -59,7 +59,7 @@ const start = (routeSet, passport = null) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,authorization')
     next()
   })
 
