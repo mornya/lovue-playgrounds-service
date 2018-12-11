b37abb21d05b19122050e0f5f0b8d2ea325aebc8
정상준 <mornya@wemakeprice.com>
2018-12-11 16:45:06 +0900

    인증테스트

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -2,73 +2,61 @@ import passport from 'passport'
 import BaseController from 'controllers/BaseController'
 import { getToken } from 'utils/JWT'
 
+const authProviders = ['facebook', 'google', 'kakao', 'naver']
+
 export default class AuthController extends BaseController {
   constructor (router, { UserLog }) {
     super()
 
     this.router = router
-    this.router.get('/auth/facebook', this.passportAuth('facebook', { scope: 'email' }))
-    this.router.get('/auth/facebook/callback', this.passportAuthCallback('facebook'))
-    this.router.get('/auth/google', this.passportAuth('google', { scope: ['openid', 'email'] }))
-    this.router.get('/auth/google/callback', this.passportAuthCallback('google'))
-    this.router.get('/auth/naver', this.passportAuth('naver'))
-    this.router.get('/auth/naver/callback', this.passportAuthCallback('naver'))
-    this.router.get('/auth/kakao', this.passportAuth('kakao'))
-    this.router.get('/auth/kakao/callback', this.passportAuthCallback('kakao'))
+    this.router.get('/auth/:provider', this.passportAuth)
+    this.router.get('/auth/:provider/callback', this.passportAuthCallback)
     this.router.post('/auth/signout', this.signout)
 
     this.userLogModel = UserLog
   }
 
-  setCookieForAuth = (req, res) => {
-    const { provider, redirectUrl } = req.query
-    const cookieOptions = {
-      expires: new Date(Date.now() + 300000), // 5min (5 * 60 * 1000)
-      httpOnly: false,
-    }
+  passportAuth = (req, res, next) => {
+    const { provider } = req.params
+    if (provider && authProviders.indexOf(provider) >= 0) {
+      const { redirectUrl } = req.query
+      const cookieOptions = {
+        expires: new Date(Date.now() + 300000), // 5min (5 * 60 * 1000)
+        httpOnly: false,
+      }
 
-    if (redirectUrl) {
-      res.cookie('redirectUrl', redirectUrl, cookieOptions)
-    }
-    if (!provider) {
+      if (redirectUrl) {
+        res.cookie('redirectUrl', redirectUrl, cookieOptions)
+      }
+
+      let temp, option = null
+      switch (provider) {
+        case 'facebook': {
+          temp = JSON.parse(process.env.PASSPORT_FACEBOOK_PROFILE_FIELDS)
+          option = temp && temp.length ? { scope: temp.join(',') } : null
+          break;
+        }
+        case 'google': {
+          temp = JSON.parse(process.env.PASSPORT_GOOGLE_SCOPE)
+          option = temp && temp.length ? { scope: temp } : null
+          break;
+        }
+        default:
+          break;
+      }
+      passport.authenticate(provider, option, null)(req, res, next)
+    } else {
       this.sendResponseError(res, this.responseCodes.HTTP_403) // Forbidden
     }
   }
 
-  signout = (req, res) => {
-    const { userId, provider } = req.body
-
-    if (userId && provider) {
-      // 로그아웃 기록 저장
-      this.userLogModel.findOne({ userId, provider })
-        .then((resultData) => {
-          const date = new Date()
-
-          if (resultData) {
-            // signin 기록이 없을 수 없다고 가정하고 update 처리만 한다.
-            return this.userLogModel.update({ userId, provider }, { $push: { signoutLogs: date } })
-          } else {
-            throw new Error('No user log info to write signed-out time.')
-          }
-        })
-        .then((resultData) => this.sendResponse(res, resultData))
-        .catch((err) => this.logWarn(err)) // 실패 오류 및 후속 처리는 아무것도 없음
-    } else {
-      this.sendResponse(res, {})
-    }
-  }
-
-  passportAuth = (provider, option) => (req, res, next) => {
-    this.setCookieForAuth(req, res)
-    passport.authenticate(provider, option, null)(req, res, next)
-  }
-
-  passportAuthCallback = (provider) => (req, res, next) => {
-    (
-      passport.authenticate(provider, (err, user/*, info*/) => {
+  passportAuthCallback = (req, res, next) => {
+    const { provider } = req.params
+    if (provider && authProviders.indexOf(provider) >= 0) {
+      const passportCallback = (err, user/*, info*/) => {
         const redirectUrl = (req.cookies && req.cookies.redirectUrl) || ''
         const origin = process.env.URL_ORIGIN_UI
-
+        console.warn('REDIRECT TO >>>', redirectUrl)
         res.clearCookie('redirectUrl') // always clear cookie
 
         if (err) {
@@ -98,7 +86,33 @@ export default class AuthController extends BaseController {
                 origin,
               }))
         }
-      }, null)
-    )(req, res, next)
+      }
+      passport.authenticate(provider, passportCallback, null)(req, res, next)
+    } else {
+      this.sendResponseError(res, this.responseCodes.HTTP_403) // Forbidden
+    }
+  }
+
+  signout = (req, res) => {
+    const { userId, provider } = req.body
+
+    if (userId && provider) {
+      // 로그아웃 기록 저장
+      this.userLogModel.findOne({ userId, provider })
+        .then((resultData) => {
+          const date = new Date()
+
+          if (resultData) {
+            // signin 기록이 없을 수 없다고 가정하고 update 처리만 한다.
+            return this.userLogModel.update({ userId, provider }, { $push: { signoutLogs: date } })
+          } else {
+            throw new Error('No user log info to write signed-out time.')
+          }
+        })
+        .then((resultData) => this.sendResponse(res, resultData))
+        .catch((err) => this.logWarn(err)) // 실패 오류 및 후속 처리는 아무것도 없음
+    } else {
+      this.sendResponse(res, {})
+    }
   }
 }
